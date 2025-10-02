1ee8d3d1f560de8021940e05eeab7917c5fde329
Omkar makani <85292542+Omkarmakani0003@users.noreply.github.com>
2025-10-02 17:17:04 +0530

    Update script.js

--- SimpleTodo/script.js
+++ SimpleTodo/script.js
@@ -4,7 +4,15 @@ function addTodo(){
     id++
     let task = document.getElementById('task-input').value
     if(task != ""){
-       list.innerHTML += `<li id="${id}"><p>${task}</p><button class="remove" onclick="remove(${id})">remove</button></li>`
+       list.innerHTML += `
+            <li id="${id}">
+                <p>${task}</p>
+                <div class="btn">
+                  <button class="edit" onclick="edit(${id})">edit</button>
+                  <button class="remove" onclick="remove(${id})">remove</button>
+                </div>  
+            </li>
+            `
        document.getElementById('task-input').value = ''
        document.querySelector('.errorMsg').innerHTML = ''
     }else{
@@ -15,4 +23,27 @@ function addTodo(){
 
 function remove(id){
     document.getElementById(id).remove()
+}
+
+function edit(id){
+  let ele = document.getElementById(id)
+  ele.innerHTML = `
+      <input type="text" id="new-input" placeholder="Edit task" value="${ele.children[0].innerText}">
+      <div class="btn">
+        <button class="edit" onclick="save(${id})">save</button>
+        <button class="remove" onclick="remove(${id})">remove</button>
+      </div>  
+  `
+}
+
+function save(id){
+    let ele = document.getElementById(id)
+    let newInput = document.getElementById('new-input').value
+    ele.innerHTML = `
+      <p>${newInput}</p>
+      <div class="btn">
+        <button class="edit" onclick="edit(${id})">edit</button>
+        <button class="remove" onclick="remove(${id})">remove</button>
+      </div>  
+  `
 }
